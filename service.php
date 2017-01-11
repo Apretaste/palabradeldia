f6a98dd8803830bafd9f1596e8cff580c23ba172
kumahacker <kumahavana@gmail.com>
2017-01-10 23:59:59 -0500

    fixes

--- service.php
+++ service.php
@@ -1,43 +1,85 @@
 <?php
-  use Goutte\Client;
-  class palabradeldia extends Service {
+use Goutte\Client;
+
+/**
+ * PalabraDelDia
+ *
+ * Apretaste's service
+ *
+ * @author kuma <kumahavana@gmail.com>
+ * @version 1.0
+ * @updated 2017-01-10
+ */
+class palabradeldia extends Service
+{
     public function _main(Request $request)
     {
-      $response = new Response();
-      $response->setResponseSubject("Palabra en Inglés del día");
-      $response->createFromTemplate("basic.tpl", $this->getWord());
-      return $response;
+
+        // create a new Client
+        $client = new Client();
+        $guzzle = $client->getClient();
+        $client->setClient($guzzle);
+
+        // create a Crawler
+        $crawler = $client->request('GET', 'http://feeds.feedblitz.com/english-word-of-the-day-for-spanish&x=1');
+        
+        // get the title
+        $title = $crawler->filter('item title')->text();
+
+        // get the text
+        $text = $crawler->filter('item description')->text();
+
+        // and strip style
+        $description = preg_replace('/(<[^>]+) style=".*?"/i', '$1', $text);
+
+        /*$description = str_ireplace(
+                ['Part of speech', 'Example sentence', 'Sentence meaning', '<table','<th', 'adjective', 'sustantive'],
+                ['Parte del habla', 'Oraci&oacute;n de ejemplo', 'Significado de la oraci&oacute;n','<table width="100%"', '<th align="right"', 'adjetivo','sustantivo'],
+            $description
+        );*/
+        $description = $this->parse($description);
+
+        $title = explode(":", $title);
+        $title[0] = trim($title[0]);
+        $title[1] = trim($title[1]);
+
+        $description = get_object_vars($description->data);
+        $description['Example sentence'] = str_replace($title[0],'<b>' . $title[0] . '</b>', $description['Example sentence']);
+        $description['Sentence meaning'] = str_replace($title[1],'<b>' . $title[1] . '</b>', $description['Sentence meaning']);
+
+        $content = array(
+            'title' => $title,
+            'description' => $description
+        );
+
+        $response = new Response();
+        $response->setResponseSubject("Palabra en Inglés del día");
+        $response->createFromTemplate("basic.tpl", $content);
+        return $response;
     }
 
-    /**
-     * Get the English Word of the Day from
-     * Transparent
-     * @link http://www.transparent.com/word-of-the-day/today/english.html
-     * @return Array
-     */
-
-    private function getWord()
+    public function parse($text)
     {
-      // create a new Client
-      $client = new Client();
-      $guzzle = $client->getClient();
-      $client->setClient($guzzle);
+        $text = trim($text);
+        $text = str_replace('<table>', '{"data":{', $text);
+        $text = str_replace('</table>', '}}', $text);
 
-      // create a Crawler
-      $crawler = $client->request('GET', 'http://feeds.feedblitz.com/english-word-of-the-day-for-spanish&x=1');  
 
-      // get the title
-      $title = $crawler->filter('item title')->text();
+        $text = str_replace('<th', '"<th', $text);
+        $text = str_replace(':</th>', '":</th>', $text);
 
-      // get the text
-      $text = $crawler->filter('item description')->text();
-	
-      // and strip style 
-      $description = preg_replace('/(<[^>]+) style=".*?"/i', '$1', $text);
+        $text = str_replace('<td', '"<td', $text);
+        $text = str_replace('</td>', '"</td>,', $text);
 
-      return array(
-        'title' => $title,
-        'description' => $description
-      );
+        $text = strip_tags($text);
+        $text = str_replace("\r", "",$text);
+        $text = str_replace("\n", "",$text);
+
+        $text = str_replace(",}}","}}", $text);
+
+        $text = htmlentities($text);
+        $text = str_replace('&quot;', '"', $text);
+
+        return @json_decode($text);
     }
-  } 
+}
--- templates/basic.tpl
+++ templates/basic.tpl
@@ -1,7 +1,10 @@
-<center>
-  <h1>La palabra del d&iacute;a es:</h1>
-  <h2>{$title}</h2>
-  <div>
-    {$description}
-  </div>
-</center>
+<h1>La palabra del d&iacute;a es:</h1>
+{button href="TRADUCIR {$title[0]}" caption ="{$title[0]}" size="small" } = {button href="TRADUCIR INGLES {$title[1]}" size="small" caption ="{$title[1]}" color="blue"}
+{space5}
+<h3>Ejemplo en Ingl&eacute;s</h3>
+<p>{$description['Example sentence']}</p>
+<h3>Ejemplo en Espa&ntilde;ol</h3>
+<p>{$description['Sentence meaning']}</p>
+
+
+
